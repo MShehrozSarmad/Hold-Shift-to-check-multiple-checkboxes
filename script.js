0feed69a4e534d752ddeb2e36fcaa84e2a512615
Muhammad Shehroz Sarmad <64995399+MrHacker0001@users.noreply.github.com>
2022-05-18 18:18:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleChecks(e){
+    let inBetween = false;
+
+    if(e.shiftKey && this.checked){
+        checkBoxes.forEach(checkBox => {
+            if(checkBox === this || checkBox === lastChecked){
+                inBetween =! inBetween;
+            }
+
+            if(inBetween){
+                checkBox.checked = true;
+            }
+        });
+    }
+
+    lastChecked = this;
+}
+
+
+checkBoxes.forEach(checkBox => checkBox.addEventListener('click', handleChecks));
